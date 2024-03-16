6aca620ae63e4f074c7851bc24a793c4a0caf04b
ahmedhossam1212 <pmedo1689@gmail.com>
2024-03-16 12:34:05 +0200

    finish committees

--- lib/core/static/care_committees/solidarity_fund.dart
+++ lib/core/static/care_committees/solidarity_fund.dart
@@ -1,6 +1,7 @@
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:student_hub/core/utils/app_colors.dart';
+import 'package:student_hub/core/utils/media_query_values.dart';
 import 'package:student_hub/core/utils/style_manager.dart';
 
 class SolidarityFundScreen extends StatelessWidget {
@@ -9,18 +10,49 @@ class SolidarityFundScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(EneftyIcons.arrow_left_3_outline)),
-        title: Text(
-          "صندوق التكافل",
-          style: getBoldStyle(color: AppColors.black, fontSize: 20),
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(EneftyIcons.arrow_left_3_outline)),
+          title: Text(
+            "صندوق التكافل ",
+            style: getBoldStyle(color: AppColors.black, fontSize: 20),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-    );
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Column(
+                children: [
+                  Text(
+                    "صندوق التكافل",
+                    style: getBoldStyle(color: AppColors.black, fontSize: 15),
+                  ),
+                  Container(
+                    width: context.width * 0.36,
+                    height: 1,
+                    color: AppColors.primary,
+                  ),
+                  SizedBox(
+                    height: context.height * 0.02,
+                  ),
+                  Text(
+                    "اكتشاف المواهب الفنيه بين الطلاب وتنميتها وتشجيعها كذلك اقامه المعارض الفنيه والحفلات لاظهار مواهب الطلاب وتشجيعهم",
+                    style: getBoldStyle(color: AppColors.grey, fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: context.height * 0.02,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
--- lib/core/static/care_committees/travelers_publicservice_committee.dart
+++ lib/core/static/care_committees/travelers_publicservice_committee.dart
@@ -1,6 +1,7 @@
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:student_hub/core/utils/app_colors.dart';
+import 'package:student_hub/core/utils/media_query_values.dart';
 import 'package:student_hub/core/utils/style_manager.dart';
 
 class TravelerPublicServiceCommitteeScreen extends StatelessWidget {
@@ -16,10 +17,165 @@ class TravelerPublicServiceCommitteeScreen extends StatelessWidget {
             },
             icon: const Icon(EneftyIcons.arrow_left_3_outline)),
         title: Text(
-          "لجنة الجوالة والخدمة العامة",
+          "لجنه الجواله والخدمه العامه",
           style: getBoldStyle(color: AppColors.black, fontSize: 20),
         ),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Column(
+              children: [
+                Text(
+                  "انشطه لجنه الجواله",
+                  style: getBoldStyle(color: AppColors.black, fontSize: 15),
+                ),
+                Container(
+                  width: context.width * 0.36,
+                  height: 1,
+                  color: AppColors.primary,
+                ),
+                SizedBox(
+                  height: context.height * 0.02,
+                ),
+                Text(
+                  "اكتشاف المواهب الفنيه بين الطلاب وتنميتها وتشجيعها كذلك اقامه المعارض الفنيه والحفلات لاظهار مواهب الطلاب وتشجيعهم",
+                  style: getBoldStyle(color: AppColors.grey, fontSize: 15),
+                ),
+                SizedBox(
+                  height: context.height * 0.02,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: context.width * 0.9,
+                        child: Card(
+                            clipBehavior: Clip.hardEdge,
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/dawrat.png",
+                                  height: context.height * 0.4,
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "تداول توعيه للجواله",
+                                        style: getMediumStyle(
+                                            color: AppColors.black,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        width: context.width * 0.9,
+                        child: Card(
+                            clipBehavior: Clip.hardEdge,
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/kashafa.png",
+                                  height: context.height * 0.4,
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "المشاركه فى الدوره الكشافيه على مستوى الجامعه",
+                                        style: getMediumStyle(
+                                            color: AppColors.black,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        width: context.width * 0.9,
+                        child: Card(
+                            clipBehavior: Clip.hardEdge,
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/night.jpg",
+                                  height: context.height * 0.4,
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "حفلات سهر",
+                                        style: getMediumStyle(
+                                            color: AppColors.black,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        width: context.width * 0.9,
+                        child: Card(
+                            clipBehavior: Clip.hardEdge,
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  "assets/images/yadwy.jpg",
+                                  height: context.height * 0.4,
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "ورشه مشغولات يدويه",
+                                        style: getMediumStyle(
+                                            color: AppColors.black,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
